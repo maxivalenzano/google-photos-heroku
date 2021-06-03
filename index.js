8158d69a858311b7445b327790fa02709602c0a3
Maximiliano <maxivalenzano@gmail.com>
2021-06-03 16:46:27 -0300

    correcction

--- index.js
+++ index.js
@@ -3,12 +3,27 @@ const app = express()
 const { getAlbum } = require('./google-photos')
 const port = process.env.PORT || 8081;
 
+const allowedOrigins = ['http://localhost:3000', 'http://localhost:3001', 'https://xio-y-maxi.netlify.app']
+
+app.use(function (req, res, next) {
+  const origin = req.headers.origin
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin)
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+    next()
+  } else {
+    return res.status(403).json({
+      status: 403,
+      message: 'This API is only for https://xio-y-maxi.netlify.app',
+    })
+  }
+})
 
 app.get('/', (req, res) => {
   res.send('Ta funcionando josha!')
 })
 
-app.get('/id/:id', async (req, res) => {
+app.get('/:id', async (req, res) => {
   try {
     const results = await getAlbum(req.params.id)
     res.json(results)
