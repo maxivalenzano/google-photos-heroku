fc88a805af9caab29475121c4937c12e25fca0e6
Maximiliano <maxivalenzano@gmail.com>
2021-06-03 15:16:22 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const { getAlbum } = require('./google-photos')
+const port = process.env.PORT || 8081;
+
+
+app.get('/', (req, res) => {
+  res.send('Ta funcionando josha!')
+})
+
+app.get('/:id', async (req, res) => {
+  try {
+    const results = await getAlbum(req.params.id)
+    res.json(results)
+  } catch (e) {
+    res.status(500)
+  }
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
